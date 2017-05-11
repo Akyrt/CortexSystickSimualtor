49b0f6ee84357b1a76eca48ba4017ef125f00fcc
Akyrt <akyrGit@gmail.com>
2017-05-11 18:34:19 +0200

    Added: - slider to set up a delay between current and next impulse - slider to set up a package of impulses for BURST mode - slider showing current CVR state

--- src/com/example/progwjavie/SysTickGUI.java
+++ src/com/example/progwjavie/SysTickGUI.java
@@ -14,12 +14,15 @@ import java.awt.event.WindowEvent;
 public class SysTickGUI extends JFrame implements ActionListener {
     // instance variables - replace the example below with your own
     JButton oneImp, kImp;
-    JTextField rejRVR, rejCVR, enterkImp, rejSetCVR, rejSetRVR, interrupt, stanImp;
-    JRadioButton enable, count, tickInt, genOnOFF;
+    JTextField rejRVR, rejCVR, enterkImp, regSetCVR, regSetRVR, interrupt, stateImp, stateBurstImp, stateDelayTime;
+    JRadioButton enable, count, tickInt, genOnOFF, modeSet;
+    Integer bImp;
     int kNumber;
     private licznik_SysTick myDemoCounter;
     PulseGenerator generate;
-    JTextField howManyBurstImpulse;
+    JSlider sliderBurstMode, sliderSetDelay, sliderTimer;
+
+    //JTextField howManyBurstImpulse;
 
     /**
      * Constructor for objects of class SysTickGUI
@@ -27,21 +30,19 @@ public class SysTickGUI extends JFrame implements ActionListener {
     public SysTickGUI() {
         myDemoCounter = new licznik_SysTick();
 
-        setSize(500, 500);
+        // setSize(500, 500);
         setResizable(false);    // blokuje możliwość powiększania okna
         setVisible(true);
         makeGUI();
         generate = new PulseGenerator();
         generate.addActionListener(this);
         generate.setPulseCount(5);
-
+        pack();
 
 
     }
 
     public void makeGUI() {
-
-        // FlowLayout , GridLayout, BorderLayout
         setLayout(new BorderLayout());
 
         JPanel pGorny, pDolny, pCentrum, pLewy, pPrawy; // deklaracja obiektów klasy JPanel do podziału okna na części
@@ -51,7 +52,6 @@ public class SysTickGUI extends JFrame implements ActionListener {
         pGorny.setBackground(Color.YELLOW);             // ustalenie koloru tła części górnej okna
         add(pGorny, BorderLayout.NORTH);                // ustawienie w oknie górnej części
         JLabel etName = new JLabel(" Symulator licznika CortexM0 SysTick ");    // inicjalizacja obiektu etName wraz z zawartością
-        // etName.setIcon(new ImageIcon("D:\\Bluej\\Workspace\\210090\\Systick\\Systick\\ic_alarm.png"));
         etName.setIcon(new ImageIcon("D:\\IntelliJ\\IntelliJ_Workspace\\licznik_SysTick\\icons\\ic_alarm.png")); // ustawienie ikony
         pGorny.add(etName);
 
@@ -67,7 +67,6 @@ public class SysTickGUI extends JFrame implements ActionListener {
         pLewy.add(etRVR);
         // 2 wiersz
         JLabel wpiszCVR = new JLabel("");
-        // wpiszCVR.setIcon(new ImageIcon("D:\\Bluej\\Workspace\\210090\\Systick\\Systick\\ic_settings.png"));
         wpiszCVR.setIcon(new ImageIcon("D:\\IntelliJ\\IntelliJ_Workspace\\licznik_SysTick\\icons\\ic_settings.png"));
 
         pLewy.add(wpiszCVR);
@@ -75,17 +74,16 @@ public class SysTickGUI extends JFrame implements ActionListener {
 
         pLewy.add(wpiszRVR);
         // 3 wiersz
-        rejSetCVR = new JTextField(4);  // ustawienie pola tekstowego do wpisania wartości rejestru CVR
-        rejSetCVR.setToolTipText(" Wpisz wartość startową rejestru SYST_CVR, a następnie wciśnij Enter. "); // wyświetlenie podpowiedzi po najechaniu na pole textowe
-        rejSetCVR.addActionListener(this);
-        pLewy.add(rejSetCVR);
-        rejSetRVR = new JTextField(4);
-        rejSetRVR.setToolTipText(" Wpisz wartość rejestru SYST_RVR, a następnie wciśnij Enter. Od tej wartości zliczane są impulsy przesyłane do licznika. ");
-        rejSetRVR.addActionListener(this);
-        pLewy.add(rejSetRVR);
+        regSetCVR = new JTextField(4);  // ustawienie pola tekstowego do wpisania wartości rejestru CVR
+        regSetCVR.setToolTipText(" Wpisz wartość startową rejestru SYST_CVR, a następnie wciśnij Enter. "); // wyświetlenie podpowiedzi po najechaniu na pole textowe
+        regSetCVR.addActionListener(this);
+        pLewy.add(regSetCVR);
+        regSetRVR = new JTextField(4);
+        regSetRVR.setToolTipText(" Wpisz wartość rejestru SYST_RVR, a następnie wciśnij Enter. Od tej wartości zliczane są impulsy przesyłane do licznika. ");
+        regSetRVR.addActionListener(this);
+        pLewy.add(regSetRVR);
         // 4 wiersz
         JLabel stanRVR = new JLabel(" ");
-        // stanRVR.setIcon(new ImageIcon("D:\\Bluej\\Workspace\\210090\\Systick\\Systick\\ic_update.png"));
         stanRVR.setIcon(new ImageIcon("D:\\IntelliJ\\IntelliJ_Workspace\\licznik_SysTick\\icons\\ic_update.png"));
         pLewy.add(stanRVR);
         JLabel stanCVR = new JLabel("Stan:", SwingConstants.CENTER);
@@ -95,16 +93,23 @@ public class SysTickGUI extends JFrame implements ActionListener {
         rejCVR = new JTextField(4);
         rejCVR.setPreferredSize(new Dimension(50, 50));
         rejCVR.setToolTipText(" Wyświetla aktualny stan rejestru SYST_CVR licznika. ");
+        rejCVR.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        rejCVR.setEditable(false);
         pLewy.add(rejCVR);
         rejRVR = new JTextField(4);
+        rejRVR.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        rejRVR.setEditable(false);
         rejRVR.setToolTipText(" Wyświetla aktualny stan rejestru SYST_RVR licznika. ");
         pLewy.add(rejRVR);
 
         /*************** Panel centrum********************/
 
+
         pCentrum = new JPanel();
+
+
         add(pCentrum, BorderLayout.CENTER);
-        pCentrum.setLayout(new GridLayout(4, 2));
+        pCentrum.setLayout(new GridLayout(7, 2));
         pCentrum.setBackground(Color.ORANGE);
         // 1 wiersz
         JLabel nic = new JLabel(" ");
@@ -119,6 +124,8 @@ public class SysTickGUI extends JFrame implements ActionListener {
         enable = new JRadioButton();
         enable.setToolTipText("Zaznacz aby uaktywnić bit EnableFlag. ");
         enable.setBackground(Color.ORANGE);
+        enable.setOpaque(true);
+        enable.addActionListener(this);
         pCentrum.add(enable);
         // 3 wiersz
         JLabel etTick = new JLabel("TickInt", SwingConstants.CENTER);
@@ -136,6 +143,58 @@ public class SysTickGUI extends JFrame implements ActionListener {
         genOnOFF.addActionListener(this);
         pCentrum.add(genOnOFF);
 
+        // 5 wiersz
+
+        JLabel setModeLabel = new JLabel("Mode Continous/Burst ", SwingConstants.CENTER);
+        pCentrum.add(setModeLabel);
+
+        modeSet = new JRadioButton();
+        modeSet.setBackground(Color.ORANGE);
+        modeSet.addActionListener(e -> {
+            if (modeSet.isSelected()) {
+                generate.setMode(PulseSource.BURST_MODE);
+            } else generate.setMode(PulseSource.CONTINOUS_MODE);
+
+        });
+        pCentrum.add(modeSet);
+        // 6 wiersz
+        JLabel setDelayLabel = new JLabel("Ustaw opoznienie: ", SwingConstants.CENTER);
+        setDelayLabel.setToolTipText("Ustawia opoźnienie pomiędzy kolejnymi impulsami. ");
+        pCentrum.add(setDelayLabel);
+
+        JLabel setBurstLabel = new JLabel("Ustaw wielkość paczki: ", SwingConstants.CENTER);
+        setBurstLabel.setToolTipText("Ustawia wielkość paczki impulsów. ");
+
+        pCentrum.add(setBurstLabel);
+
+        // 7 wiersz
+        sliderSetDelay = new JSlider(0, 10000, 1);
+        sliderSetDelay.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        sliderSetDelay.setBackground(Color.ORANGE);
+        sliderSetDelay.addChangeListener(e -> {
+            Integer delayTime = sliderSetDelay.getValue();
+
+            generate.setPulseDelay(delayTime);
+            sliderSetDelay.setToolTipText("Opoznienie: " + delayTime.toString() + " ms.");
+            stateDelayTime.setText(delayTime.toString());
+
+        });
+        pCentrum.add(sliderSetDelay);
+
+
+        sliderBurstMode = new JSlider(0, 1000, 1);
+        sliderBurstMode.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        sliderBurstMode.setBackground(Color.ORANGE);
+        sliderBurstMode.addChangeListener(e -> {
+            bImp = sliderBurstMode.getValue();
+            generate.setPulseCount(bImp);
+            sliderBurstMode.setToolTipText("Paczka: " + bImp.toString() + " impulsow.");
+            stateBurstImp.setText(bImp.toString());
+        });
+
+        pCentrum.add(sliderBurstMode);
+
+
         /*************** Panel prawy ***********************/
         pPrawy = new JPanel();
         add(pPrawy, BorderLayout.EAST);
@@ -145,23 +204,33 @@ public class SysTickGUI extends JFrame implements ActionListener {
         JLabel etInterrupt = new JLabel("Stan przerwania ", SwingConstants.CENTER);
         pPrawy.add(etInterrupt);
         interrupt = new JTextField();
+        interrupt.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        interrupt.setEditable(false);
         pPrawy.add(interrupt);
         // 2 wiersz
+        JLabel etTimer = new JLabel("Stan licznika", SwingConstants.CENTER);
+        pPrawy.add(etTimer);
+
+        sliderTimer = new JSlider();
+        sliderTimer.setValue(myDemoCounter.getCVR());
+        sliderTimer.setBackground(Color.CYAN);
+        sliderTimer.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        pPrawy.add(sliderTimer);
+
+        // 3 wiersz
         JLabel etCount = new JLabel("CountFlag", SwingConstants.CENTER);
         etCount.setToolTipText("CountFlag zwraca 1 gdy SysTick zliczy do 0. \n Ustawia się na 0 w momencie odczytania z niego, jego wartości oraz wpisania wartości do rejestru CVR. ");
         pPrawy.add(etCount);
         count = new JRadioButton();
         count.setBackground(Color.RED);
         pPrawy.add(count);
-        enable.addActionListener(this);
-
-        howManyBurstImpulse = new JTextField("Set burst impulse",SwingConstants.CENTER);
+/*
+        howManyBurstImpulse = new JTextField("Set burst impulse", SwingConstants.CENTER);
         howManyBurstImpulse.addActionListener(e -> {
             int bImp = Integer.parseInt(howManyBurstImpulse.getText());
             generate.setPulseCount(bImp);
         });
-        pPrawy.add(howManyBurstImpulse);
-
+        pPrawy.add(howManyBurstImpulse);*/
 
 
         /*************** Panel dolny **********************/
@@ -171,21 +240,43 @@ public class SysTickGUI extends JFrame implements ActionListener {
         oneImp = new JButton(" Pojedyńczy impuls ");
         oneImp.setToolTipText(" Po naciśnięciu przesyła jeden impuls to licznika SysTick. ");
         oneImp.addActionListener(this);
+        oneImp.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         pDolny.add(oneImp);
 
         kImp = new JButton(" K impulsów ");
         kImp.setToolTipText("Po naciśnięciu tego przycisku wysyłasz K inpulsów do licznika SysTick. "); // ustawia chmurkę wyswietlajaca sie po najechaniu kursorem na klawisz
         kImp.addActionListener(this);
+        kImp.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         pDolny.add(kImp);
         enterkImp = new JTextField(3);
+        enterkImp.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         enterkImp.setToolTipText("Wpisz ilość impulsów które chcesz wysłać a następnie wciśnij Enter. ");
         enterkImp.addActionListener(this);
         pDolny.add(enterkImp);
 
         JLabel etStanImp = new JLabel("Stan impulsów:");
         pDolny.add(etStanImp);
-        stanImp = new JTextField(3);
-        pDolny.add(stanImp);
+        stateImp = new JTextField(3);
+        stateImp.setEditable(false);
+        stateImp.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        pDolny.add(stateImp);
+
+        JLabel dT = new JLabel("Opoznienie: ");
+        pDolny.add(dT);
+
+        stateDelayTime = new JTextField(4);
+        stateDelayTime.setEditable(false);
+        stateDelayTime.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        pDolny.add(stateDelayTime);
+
+        JLabel bI = new JLabel("Burst: ");
+        pDolny.add(bI);
+
+        stateBurstImp = new JTextField(3);
+        stateBurstImp.setEditable(false);
+        stateBurstImp.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        pDolny.add(stateBurstImp);
+
 
         addWindowListener(new WindowAdapter() {
             @Override
@@ -202,9 +293,10 @@ public class SysTickGUI extends JFrame implements ActionListener {
 
     void displayState() {
         rejCVR.setText("" + myDemoCounter.getCVR());
-        stanImp.setText("" + myDemoCounter.getCVR());
+        stateImp.setText("" + myDemoCounter.getCVR());
         rejRVR.setText("" + myDemoCounter.getRVR());
         count.setSelected(myDemoCounter.isCountFlag());
+        sliderTimer.setValue(myDemoCounter.getCVR());
         if (myDemoCounter.getInterrupt() == true) {
             interrupt.setText(" ** Przerwanie ** ");
             interrupt.setBackground(Color.ORANGE);
@@ -218,6 +310,7 @@ public class SysTickGUI extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == oneImp) {       // warunek sprawdzajacy czy zostal klikniety Pojedyńczy impuls
             myDemoCounter.impuls();
+            sliderTimer.setValue(myDemoCounter.getCVR());
             rejCVR.setText(" " + myDemoCounter.getCVR());
         }
         if (e.getSource() == enable) {       // warunek sprawdzajacy czy zostal zaznaczony radiobutton EnableFlag
@@ -229,6 +322,7 @@ public class SysTickGUI extends JFrame implements ActionListener {
         if (e.getSource() == kImp) {
             for (int i = 0; i <= kNumber; i++) {
                 myDemoCounter.impuls();
+
             }
             displayState();
 
@@ -238,12 +332,12 @@ public class SysTickGUI extends JFrame implements ActionListener {
 
         }
 
-        if (e.getSource() == rejSetCVR) {
-            myDemoCounter.setCVR(Integer.parseInt(rejSetCVR.getText()));
+        if (e.getSource() == regSetCVR) {
+            myDemoCounter.setCVR(Integer.parseInt(regSetCVR.getText()));
         }
 
-        if (e.getSource() == rejSetRVR) {
-            myDemoCounter.setRVR(Integer.parseInt(rejSetRVR.getText()));
+        if (e.getSource() == regSetRVR) {
+            myDemoCounter.setRVR(Integer.parseInt(regSetRVR.getText()));
 
         }
 
@@ -252,12 +346,11 @@ public class SysTickGUI extends JFrame implements ActionListener {
         }
         if (e.getSource() == genOnOFF) {
             boolean gState = genOnOFF.isSelected();
-            if (gState == true) {
-                generate.setMode(PulseSource.BURST_MODE);
-                generate.trigger();
-            }
-            else generate.halt();
 
+            if (gState == true) {
+                //generate.setMode(PulseSource.BURST_MODE);
+                generate.trigger();
+            } else generate.halt();
 
 
         }
